e3934c1e79b0caee0877cfe701c1939c49c23f7d
David Nedrow <dnedrow@me.com>
2025-05-19 23:41:48 -0400

    chore: Added skeleton project

---
+++ WeatherPlus/WeatherPlusTests/WeatherPlusTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherPlusTests.swift
+//  WeatherPlusTests
+//
+//  Created by David Nedrow on 5/19/25.
+//
+
+import Testing
+@testable import WeatherPlus
+
+struct WeatherPlusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
